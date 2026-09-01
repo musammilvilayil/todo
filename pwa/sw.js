ac14fd640f43cb8cae776a847aac749b8cd510a5
musammilvilayil <140297924+musammilvilayil@users.noreply.github.com>
2026-09-01 16:42:40 +0530

    Cache StudyForge 3.2 human rhythm scheduler

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,5 +1,5 @@
-const CACHE='studyforge-pwa-v31';
-const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='studyforge-pwa-v32';
+const ASSETS=['./','./index.html','./app.js','./scheduler-v32.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
