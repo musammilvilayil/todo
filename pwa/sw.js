4a5eb8e9ad23cf937be7c6d69b5de7c397704b8f
musammilvilayil <140297924+musammilvilayil@users.noreply.github.com>
2026-09-01 17:49:13 +0530

    Cache automatic recovery v4.3

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,5 +1,5 @@
-const CACHE='studyforge-pwa-v42';
-const ASSETS=['./','./index.html','./app.js','./scheduler-v32.js','./live-day-v33.js','./pause-v34.js','./personal-os-v40.js','./personal-os-v40.css','./offday-v41.js','./offday-v41.css','./home-offday-v42.js','./live-day-v33.css','./manifest.webmanifest','./icon.svg'];
+const CACHE='studyforge-pwa-v43';
+const ASSETS=['./','./index.html','./app.js','./scheduler-v32.js','./live-day-v33.js','./pause-v34.js','./personal-os-v40.js','./personal-os-v40.css','./offday-v41.js','./offday-v41.css','./home-offday-v42.js','./recovery-v43.js','./live-day-v33.css','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
