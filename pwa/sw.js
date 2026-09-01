b88a82a1b5b16b56a2c9a54c21d2f2e669632016
musammilvilayil <140297924+musammilvilayil@users.noreply.github.com>
2026-09-01 15:39:24 +0530

    Cache StudyForge AI Planner assets offline

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,5 +1,5 @@
-const CACHE='studyforge-pwa-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+const CACHE='studyforge-pwa-v3';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
@@ -8,11 +8,9 @@ self.addEventListener('activate',event=>{
 });
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
-  event.respondWith(
-    fetch(event.request).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-      return response;
-    }).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html')))
-  );
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));
 });
