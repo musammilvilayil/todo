b02caa363aaae6f583e895e6126035dcac2c7ce6
musammilvilayil <140297924+musammilvilayil@users.noreply.github.com>
2026-09-01 17:35:05 +0530

    Cache StudyForge Off Day v4.1 assets

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,5 +1,5 @@
-const CACHE='studyforge-pwa-v40';
-const ASSETS=['./','./index.html','./app.js','./scheduler-v32.js','./live-day-v33.js','./pause-v34.js','./personal-os-v40.js','./personal-os-v40.css','./live-day-v33.css','./manifest.webmanifest','./icon.svg'];
+const CACHE='studyforge-pwa-v41';
+const ASSETS=['./','./index.html','./app.js','./scheduler-v32.js','./live-day-v33.js','./pause-v34.js','./personal-os-v40.js','./personal-os-v40.css','./offday-v41.js','./offday-v41.css','./live-day-v33.css','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
